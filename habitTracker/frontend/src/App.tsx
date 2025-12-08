9c256d9bf8e2c1789f37b0fe61b1cb481a6ec4cb
Alyosha <alohahoy@gmail.com>
2025-12-08 14:06:51 +0000

    Add proxy

--- habitTracker/frontend/src/App.tsx
+++ habitTracker/frontend/src/App.tsx
@@ -22,14 +22,14 @@ function App() {
   useEffect(() => {
     // fetch habits from the backend and set the state
     const fetchHabits = async () => {
-      fetch(`${import.meta.env.VITE_SERVER_URL}/habits/habits`)
+      fetch(`/api/habits/habits`)
         .then(response => response.json())
         .then(data => setHabitsState(data.habits))
         .catch(error => console.error('Error fetching habits:', error));
     }
 
     const fetchTodos = async () => {
-      fetch(`${import.meta.env.VITE_SERVER_URL}/todos/all`)
+      fetch(`/api/todos/all`)
         .then(response => response.json())
         .then(data => setToDosState(data.todos))
         .catch(error => console.error('Error fetching habits:', error));
@@ -45,7 +45,7 @@ function App() {
     
 
     // make fetch request to the backend to update the logged hours for the habit with the given id
-    const response = await fetch(`${import.meta.env.VITE_SERVER_URL}/habits/log/${id}`, {
+    const response = await fetch(`/api/habits/log/${id}`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -80,7 +80,7 @@ function App() {
     setToDosState([...toDosState, newTask]);
 
     // Make fetch request to backend to add new To-Do item
-    const resposne = await fetch(`${import.meta.env.VITE_SERVER_URL}/todos/add`, {
+    const resposne = await fetch(`/api/todos/add`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -111,7 +111,7 @@ function App() {
       );
     
     // Make fetch request to backend to mark To-Do item as complete
-    const response = await fetch(`${import.meta.env.VITE_SERVER_URL}/todos/complete/${id}`, {
+    const response = await fetch(`/api/todos/complete/${id}`, {
       method: 'PUT',
       headers: {
         'Content-Type': 'application/json',
--- habitTracker/frontend/vite.config.ts
+++ habitTracker/frontend/vite.config.ts
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy:
+        mode === 'development'
+          ? {
+              '/api': {
+                target: env.VITE_SERVER_URL,
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, ''),
+              },
+            }
+          : undefined,
+    },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+    }
+});
